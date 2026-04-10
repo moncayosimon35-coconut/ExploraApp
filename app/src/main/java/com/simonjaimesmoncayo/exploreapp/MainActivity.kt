3b10426455c8cd96e46a09e98361a7e529d33a33
Simon Andres Jaimes Moncayo <sjaimes371@unab.edu.co>
2026-04-10 10:01:59 -0500

    Configuracion de la Navegación

--- app/src/main/java/com/simonjaimesmoncayo/exploreapp/MainActivity.kt
+++ app/src/main/java/com/simonjaimesmoncayo/exploreapp/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.simonjaimesmoncayo.exploreapp.LoginScreen
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +16,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val myNavController=rememberNavController()
+            NavHost(
+                navController = myNavController,
+                startDestination = "login",
+                modifier= Modifier.fillMaxSize()
+            ){
+                composable(route ="login"){
+                    LoginScreen(onLoginSuccess={}, onNavigateToRegister={
+                        myNavController.navigate("register")
+                    })
 
-            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+
+            }
+            composable(route="register"){
+                RegisterScreen(onRegisterSuccess={},onNavigateToLogin={},
+                onBackClick = {
+
+                })
+            }
+
+        }
 
 
 
