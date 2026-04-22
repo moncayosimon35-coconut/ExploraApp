9bc6f0983de2237bad8588f134ce0992c356ba74
moncayosimon35-coconut <moncayosimon35@gmail.com>
2026-04-22 09:50:13 -0500

    Actividad Configuracion FireBase 22 Abril 2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -48,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,5 +58,11 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    //navegation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    // firebase
+    implementation(platform(libs.firebase.bom))
+// auth
+    implementation(libs.firebase.auth)
+
 }
